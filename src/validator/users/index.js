904fe2d7580f73af7ba88aa0a3177c730fd843be
Hafidh Pradipta A <hamstergeek38@gmail.com>
2021-09-14 18:52:33 +0700

    implement validation for user payload

---
+++ src/validator/users/index.js
@@ -0,0 +1,14 @@
+const InvariantError = require('../../exceptions/InvariantError');
+const { UserPayloadSchema } = require('./schema');
+
+const UsersValidator = {
+  validateUserPayload: (payload) => {
+    const validation = UserPayloadSchema.validate(payload);
+
+    if (validation.error) {
+      throw new InvariantError(validation.error.message);
+    }
+  },
+};
+
+module.exports = UsersValidator;
