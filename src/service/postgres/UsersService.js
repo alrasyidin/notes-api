28ff52566f789f51955179b8f742d5d64924df7c
Hafidh Pradipta A <hamstergeek38@gmail.com>
2021-09-15 18:44:23 +0700

    implement user authentication and verification

--- src/service/postgres/UsersService.js
+++ src/service/postgres/UsersService.js
@@ -3,6 +3,7 @@ const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
   constructor() {
@@ -55,6 +56,31 @@ class UsersService {
     }
 
     return result.rows[0];
+  }
+
+  async verifyUserCredential(username, password) {
+    // const hashedPassword = bcrypt(password);
+
+    const query = {
+      text: 'SELECT * FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result;
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang anda berikan salah');
+    }
+
+    return id;
   }
 }
 
